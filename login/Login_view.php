6230f16ccf512bc28a549e158a54bcc045ec42f2
tech_babe101 <123462218+mariedoh@users.noreply.github.com>
2024-08-07 20:28:41 +0000

    Update Login_view.php

--- login/Login_view.php
+++ login/Login_view.php
@@ -8,28 +8,25 @@
     <title>AJMS</title>
     <link rel="stylesheet" href="../css/login.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <?php
+    $error = isset($_GET['error']) ? $_GET['error'] : null;?>
 </head>
 <body>
     <div class="wrapper">
         <div class="toggle-left">
             <div class="form">
                 <!-- Form to be filled when sign in is clicked -->
-                <form action="../actions/login_user_action.php" method="POST" id="LoginForm" name="Form" onsubmit="return validation()">
+                <form action="../actions/login_user_action.php" method="POST" id="LoginForm" name="LoginForm" onsubmit="return validation()">
                     <h2 style="color: rgb(146, 61, 65);">AJC MS</h2>
                     <p style="color: rgb(146, 61, 65);">Justice Made Simple and Accessible!!</p>
+                    
                     <p id="result" style="color: rgb(146, 61, 65);">
-                        <?php
-                        // Display error messages from the session
-                        session_start();
-                        if (isset($_SESSION['errors'])) {
-                            foreach ($_SESSION['errors'] as $error) {
-                                echo "<p>$error</p>";
-                            }
-                            // Clear errors after displaying
-                            unset($_SESSION['errors']);
-                        }
-                        ?>
-                    </p>
+                <?php
+                echo $error;
+                ?>
+                </p>
+                 
                     <div class="input" style="color: rgb(146, 61, 65);">
                         <input style="color: rgb(146, 61, 65); border-color:rgb(146, 61, 65)" name="email" type="text" placeholder="Email" required id="Email">
                         <i class='bx bx-user'></i>
@@ -53,25 +50,25 @@
             <img src="../images/just2.png">
         </div>
     </div>
+    
+</body>
     <!-- The validation code -->
     <script>
-        function validation() {
-            const isValidEmail = email => {
-                const re = /^(([^<>()[\\]\\.,;:\s@"]+(\.[^<>()[\\]\\.,;:\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$/;
-                return re.test(String(email).toLowerCase());
+            function validation() {
+                const isValidEmail = email => {
+                    const re = /^(([^<>()[\\]\\.,;:\s@"]+(\.[^<>()[\\]\\.,;:\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$/;
+                    return re.test(String(email).toLowerCase());
+                }
+                if (document.Form.email.value === "") {
+                    document.getElementById("result").innerHTML = "Please enter your email";
+                    return false;
+                } else if (!isValidEmail(document.Form.email.value)) {
+                    document.getElementById("result").innerHTML = "Please enter a valid email";
+                    return false;
+                } else if (document.Form.psw.value === "") {
+                    document.getElementById("result").innerHTML = "Please enter your password";
+                    return false;
+                }
             }
-
-            if (document.Form.email.value === "") {
-                document.getElementById("result").innerHTML = "Please enter your email";
-                return false;
-            } else if (!isValidEmail(document.Form.email.value)) {
-                document.getElementById("result").innerHTML = "Please enter a valid email";
-                return false;
-            } else if (document.Form.psw.value === "") {
-                document.getElementById("result").innerHTML = "Please enter your password";
-                return false;
-            }
-        }
     </script>
-</body>
 </html>
