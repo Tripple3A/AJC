dc58572db6e07e1a04fba232a3f49de202cda1b6
Anne Alwala <anne.alwala@ashesi.edu.gh>
2024-08-08 22:26:56 +0000

    change

--- admin_view/home_admin.php
+++ admin_view/home_admin.php
@@ -9,7 +9,7 @@ include '../settings/core.php';
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="chore icon.png">
+    
     <title>AJMS</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
    
