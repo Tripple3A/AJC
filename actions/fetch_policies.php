9d294fff1306b614653580779142c9ca52c9d130
Anne Alwala <anne.alwala@ashesi.edu.gh>
2024-08-09 01:27:51 +0000

    policies

--- actions/fetch_policies.php
+++ actions/fetch_policies.php
@@ -3,7 +3,7 @@
 // Fetch policies from the database
 function getPolicies(){
     include '../settings/connection.php'; // Include the database connection
-    $query = "SELECT policy_id, policy_description, last_update, policy_title, user_id FROM policy"; 
+    $query = "SELECT policy_id, policy_description, last_update, policy_title, user_id FROM Policy"; 
     $result = mysqli_query($connection, $query);
     $policies = [];
 
